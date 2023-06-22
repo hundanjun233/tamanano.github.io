d3592ac480d5a7c33dfa07bacee2b2925e35db3a
jiayuwei <hundan123011@163.com>
2023-06-22 17:00:10 +0800

    Site updated: 2023-06-22 17:00:10

--- js/hideLive2d.js
+++ js/hideLive2d.js
@@ -1,10 +1,14 @@
+
 function hideLive2d(){
     let canvas = $("canvas")
+    console.log(canvas)
+    console.log(canvas.css("display"))
+    console.log(canvas.css("opacity"))
+
     if(canvas.css("display")=="block"){
         canvas.css({"display":"none"})
     }
     else{
         canvas.css({"display":"block"})
-
     }
 }
